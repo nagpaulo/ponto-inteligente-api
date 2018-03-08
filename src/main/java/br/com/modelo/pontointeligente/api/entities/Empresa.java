1dc0a946a5139dd0deb1eb4e35c1ab929fce00e3
Paulo Roberto <nagpaulo@gmail.com>
2018-03-08 12:00:59 -0300

    Configurando Postgres

---
+++ src/main/java/br/com/modelo/pontointeligente/api/entities/Empresa.java
@@ -0,0 +1,76 @@
+package br.com.modelo.pontointeligente.api.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+public class Empresa implements Serializable{
+
+    private static final long serialVersionUID = -8738716783185539944L;
+
+    private long id;
+    private String razaoSocial;
+    private String cnpj;
+    private Date dataCriacao;
+    private Date dataAtualizacao;
+    private List<Funcionario> funcionarios;
+
+    public Empresa() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+}
---
+++ src/main/java/br/com/modelo/pontointeligente/api/entities/Funcionario.java
@@ -0,0 +1,10 @@
+package br.com.modelo.pontointeligente.api.entities;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+
+@Entity
+public class Funcionario implements Serializable{
+
+    private static final long serialVersionUID = 8303661956542576539L;
+}
