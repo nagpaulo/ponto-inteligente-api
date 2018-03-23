465834fc6d77df2c23e72f858c734888b900df52
Paulo Roberto <nagpaulo@gmail.com>
2018-03-23 17:19:22 -0300

    Implementando Cadastro PJDto

--- src/main/java/br/com/modelo/pontointeligente/api/dtos/CadastroPJDto.java
+++ src/main/java/br/com/modelo/pontointeligente/api/dtos/CadastroPJDto.java
@@ -3,6 +3,7 @@ package br.com.modelo.pontointeligente.api.dtos;
 
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
 public class CadastroPJDto {
@@ -36,6 +37,9 @@ public class CadastroPJDto {
         this.nome = nome;
     }
 
+    @NotEmpty(message = "Email não pode ser vazio")
+    @Length(min = 5, max = 200, message = "Email deve conter entre 5 e 200 caracteres.")
+    @Email(message = "Email inválido.")
     public String getEmail() {
         return email;
     }
