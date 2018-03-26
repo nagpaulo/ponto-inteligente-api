e9636eb4286900ecb61ef0c0482eb8f5e8a8b5d5
nagpaulo <nagpaulo@gmail.com>
2018-03-26 08:34:38 -0300

    Criando Controller Cadastro de Empresa.

--- src/main/java/br/com/modelo/pontointeligente/api/controller/CadastroPJController.java
+++ src/main/java/br/com/modelo/pontointeligente/api/controller/CadastroPJController.java
@@ -34,8 +34,6 @@ public class CadastroPJController {
         throws NoSuchAlgorithmException{
 
         log.info("Cadastrando PJ: {}", cadastroPJDto.toString());
-
         return null;
-
     }
 }
