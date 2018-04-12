4b87f8151705a2d2f93a67320687468ec51c2e4a
Paulo Roberto <nagpaulo@gmail.com>
2018-04-12 17:01:20 -0300

    Teste em empresa test

--- src/main/java/br/com/modelo/pontointeligente/api/entities/Funcionario.java
+++ src/main/java/br/com/modelo/pontointeligente/api/entities/Funcionario.java
@@ -187,7 +187,16 @@ public class Funcionario implements Serializable{
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
                 ", cpf='" + cpf + '\'' +
+                ", valorHora=" + valorHora +
+                ", qtdHorasTrabalhoDia=" + qtdHorasTrabalhoDia +
+                ", qtdHorasAlmoco=" + qtdHorasAlmoco +
+                ", perfil=" + perfil +
+                ", dataCriacao=" + dataCriacao +
+                ", dataAtualizacao=" + dataAtualizacao +
+                ", empresa=" + empresa +
+                ", lancamentos=" + lancamentos +
                 '}';
     }
 }
